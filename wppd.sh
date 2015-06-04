0a0b3690426cf62f0b279d1876e5245ff6c5ba5f
Larry W. Cashdollar <larry0@me.com>
2015-06-04 15:48:32 -0400

    Update wppd.sh

--- wppd.sh
+++ wppd.sh
@@ -3,15 +3,12 @@
 #Larry W. Cashdollar @_larry0
 #Usage ./wppd.sh pagenumber searchstring
 # for x in `seq 1 1400`; do ./wppd.sh $x file; done
-
 USERAGENT="\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36\""
-
-for x in `curl -sk -A $USERAGENT "https://wordpress.org/plugins/search.php?page=$1&q=$2" | egrep "href=\"https\:\/\/wordpress.org\/plugins\/[a-z]*|-\""`;do
-    Y=`        echo $x|grep href|grep -v tags`;
-if [ $Y ]; then
-    Z=`echo $Y | sed -e 's/\"/ /g' | awk '{print $2}'`;
-  echo $Z;
-	curl -sk -A $USERAGENT "$Z" |grep downloadUrl | awk -F\' '{print $4}' | xargs wget -nc
-        mv *.zip plugins
+echo "[+] Scraping Page: https://wordpress.org/plugins/search.php?page=$1&type=term&q=$2";
+for x in `curl -sk -A $USERAGENT "https://wordpress.org/plugins/search.php?page=$1&type=term&q=$2" | egrep "href=\"https\:\/\/wordpress.org\/plugins\/[a-z]*|-\""  | awk -F = '{print $2}'  | sed -e 's/"/ /g' | awk '{print $1}' | sort -u |grep -v plugin-install`;do
+#    echo $x;
+if [ $x ]; then
+        echo "+= Pulling down plugin: "
+	curl -sk -A $USERAGENT "$x" |grep downloadUrl | awk -F\' '{print $4}'  | xargs wget -nc
 fi;
 done
