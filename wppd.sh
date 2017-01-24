5fe61f0d60d786e93193bc94f0a79d105e4ca842
Larry W. Cashdollar <larry0@me.com>
2017-01-23 21:34:25 -0500

    Updated to fix a bug

--- wppd.sh
+++ wppd.sh
@@ -5,7 +5,7 @@
 # for x in `seq 1 1400`; do ./wppd.sh $x file; done
 USERAGENT="\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36\""
 echo "[+] Scraping Page: https://wordpress.org/plugins/search.php?page=$1&type=term&q=$2";
-for x in `curl -sk -A $USERAGENT "https://wordpress.org/plugins/search.php?page=$1&type=term&q=$2" | egrep "href=\"https\:\/\/wordpress.org\/plugins\/[a-z]*|-\""  | awk -F = '{print $2}'  | sed -e 's/"/ /g' | awk '{print $1}' | sort -u |grep -v plugin-install`;do
+for x in `curl -sk -A $USERAGENT "https://wordpress.org/plugins/search.php?page=$1&type=term&q=$2" | egrep "href=\"https\:\/\/wordpress.org\/plugins\/[a-z]*|-\""  | awk -F = '{print $2}'  | sed -e 's/"/ /g' | awk '{print $1}' | sort -u |grep plugins`;do
 #    echo $x;
 if [ $x ]; then
         echo "+= Pulling down plugin: "
